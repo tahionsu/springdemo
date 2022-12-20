143e815d905be1f46d3f7ea856c38eea8c529678
Anton Milov <avm@gldva.ru>
2022-12-20 14:49:34 +0700

    Add banks into person's model

--- src/main/java/com/example/spring/demo/model/Person.java
+++ src/main/java/com/example/spring/demo/model/Person.java
@@ -1,10 +1,13 @@
 package com.example.spring.demo.model;
 
+import com.example.spring.demo.entity.BankEntity;
 import com.example.spring.demo.entity.PersonEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 
 @Getter
@@ -14,12 +17,14 @@ import lombok.Setter;
 public class Person {
     private Integer id;
     private String name;
+    private List<BankEntity> banks;
 
     public static Person toModel(PersonEntity entity) {
         Person person = new Person();
 
         person.setId(entity.getId());
         person.setName(entity.getName());
+        person.setBanks(entity.getBanks());
 
         return person;
     }
