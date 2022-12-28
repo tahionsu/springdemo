b3e190553ab5ac118e0407f78fb8631277b254b0
antnraven <avm@gldva.ru>
2022-12-28 16:50:37 +0700

    Add teacher's model

---
+++ src/main/java/com/example/spring/demo/model/Teacher.java
@@ -0,0 +1,32 @@
+package com.example.spring.demo.model;
+
+import com.example.spring.demo.entity.TeacherEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Teacher {
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private String position;
+
+    Teacher toModel(TeacherEntity entity) {
+        Teacher teacher = new Teacher();
+
+        teacher.setId(entity.getId());
+        teacher.setFirstName(entity.getFirstName());
+        teacher.setLastName(entity.getLastName());
+        teacher.setPosition(entity.getPosition());
+
+        return teacher;
+    }
+}
