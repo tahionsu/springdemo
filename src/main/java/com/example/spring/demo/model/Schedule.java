7037948b9d87b3b943fdcf0da7665e39f439bff6
antnraven <avm@gldva.ru>
2022-12-28 17:20:09 +0700

    Add schedule's model

---
+++ src/main/java/com/example/spring/demo/model/Schedule.java
@@ -0,0 +1,42 @@
+package com.example.spring.demo.model;
+
+import com.example.spring.demo.entity.ScheduleEntity;
+import com.example.spring.demo.entity.SubjectEntity;
+import com.example.spring.demo.entity.TeacherEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.sql.Date;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Schedule {
+    private UUID id;
+    private SubjectEntity subject;
+    private TeacherEntity teacher;
+    private String auditory;
+    private Date dateTime;
+
+    Schedule toModel(ScheduleEntity entity) {
+        Schedule schedule = new Schedule();
+
+        schedule.setId(entity.getId());
+        schedule.setSubject(entity.getSubject());
+        schedule.setTeacher(entity.getTeacher());
+        schedule.setAuditory(entity.getAuditory());
+        schedule.setDateTime(entity.getDateTime());
+
+        return schedule;
+    }
+}
